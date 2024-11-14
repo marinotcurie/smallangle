6fda48c29fe8f0c9f16550c2442a16129b95dbc0
Mariam <mariam.sakinskaya@student.uva.nl>
2024-11-14 12:23:34 +0100

    added docstrings to the file

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,24 +3,48 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
-@click.command()
-@click.argument("number")
+@click.group()
+def cmd_group():
+    pass
+    
+@cmd_group.command()
 
 @click.option(
     "-n",
+    "--number",
+    default = 1
 )
 
+
 def sin(number):
+    """Calculates the sin values between 0 and 2pi with given amount of steps
+
+    Args:
+        number (int): the amount of steps
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default = 1
+)
+
 def tan(number):
+    """Calculates the tan values between 0 and 2pi with given amount of steps
+
+    Args:
+        number (int): the amount of steps 
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
 
 if __name__ == "__main__":
-    sin()
+    cmd_group()
+    
